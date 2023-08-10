fc405d33108d74516ac9aba87cf4706942719e14
ton1savage <81397918+ton1savage@users.noreply.github.com>
2023-08-10 09:23:24 -0700

    final fix2

--- client/src/scenes/home/Reviews.jsx
+++ client/src/scenes/home/Reviews.jsx
@@ -14,7 +14,7 @@ const Reviews = ({ apiKey }) => {
   const scrollContainerRef = useRef();
   const [showGradient, setShowGradient] = useState(true);
 
-  const BASE_URL = process.env.SITE_URL || 'http://localhost:5001'
+  const BASE_URL = process.env.SITE_URL
 
   useEffect(() => {
     fetchReviews();
