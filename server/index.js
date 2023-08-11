d6b1cd675426208a577774cca1a9dfa7693d0185
ton1savage <ethanrobinsonejr14@gmail.com>
2023-08-10 17:03:35 -0700

    url inp

--- server/index.js
+++ server/index.js
@@ -18,38 +18,44 @@ app.get('/api/reviews', async (req, res) => {
     const { apiKey } = req.query;
 
     try {
-        const response = await fetch(
-            `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${apiKey}&input=Down%20To%20The%20Details%20L.L.C.&inputtype=textquery`
-            //https://downtothedetailsllc-16467d68fd94.herokuapp.com/api/reviews?apiKey=AIzaSyBg8kkM413J-HbQpUUm0PM0FQ1hHk1jJNw&query=DownTo%20The%20Details
-        );
-
+        const googleApiUrl = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${apiKey}&input=Down%20To%20The%20Details%20L.L.C.&inputtype=textquery`;
+        const response = await fetch(googleApiUrl);
+    
         const data = await response.json();
         console.log("first api fetch: ", data);
-
+    
         if (data.candidates && data.candidates.length > 0) {
             const placeId = data.candidates[0].place_id;
-            const detailsResponse = await fetch(
-                `https://maps.googleapis.com/maps/api/place/details/json?key=${apiKey}&place_id=${placeId}&fields=reviews`
-            );
-
+            const detailsApiUrl = `https://maps.googleapis.com/maps/api/place/details/json?key=${apiKey}&place_id=${placeId}&fields=reviews`;
+            const detailsResponse = await fetch(detailsApiUrl);
+    
             const detailsData = await detailsResponse.json();
-
+    
             if (detailsData.result && detailsData.result.reviews) {
                 const reviews = detailsData.result.reviews;
-
+    
                 // Log the reviews data received on the server-side before sending to the client
                 console.log('Reviews data on the server-side:', reviews);
-
-                res.json(reviews); // Send the reviews data to the client
+    
+                // Fetch reviews from the custom API
+                const customApiUrl = `https://downtothedetailsllc-16467d68fd94.herokuapp.com/api/reviews?apiKey=${apiKey}&query=Down%20To%20The%20Details`;
+                const customApiResponse = await fetch(customApiUrl);
+                const customApiData = await customApiResponse.json();
+    
+                // Log the reviews data received from the custom API
+                console.log('Custom API Reviews data:', customApiData);
+    
+                res.json(customApiData); // Send the reviews data from the custom API to the client
                 return;
             }
         }
-
+    
         res.json([]); // Return an empty array if no reviews are found
     } catch (error) {
         console.error('Error fetching reviews:', error);
         res.status(500).json({ error: 'Error fetching reviews' });
     }
+    
 });
 
 app.use((req, res) => {
