b33d03399a1d4d954508d18ae0f238cd4bcda4d4
ton1savage <ethanrobinsonejr14@gmail.com>
2023-08-10 17:24:03 -0700

    five plus

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ app.use(express.static(path.join(__dirname, '..', 'client', 'public')))
 
 app.use(express.json());
 app.use(cors());
-// let the react app to handle any unknown routes 
-// serve up the index.html if express does'nt recognize the route
+// let the react app handle any unknown routes 
+// serve up the index.html if express doesn't recognize the route
 
 app.get('/api/reviews', async (req, res) => {
     res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, private');
@@ -20,7 +20,6 @@ app.get('/api/reviews', async (req, res) => {
     try {
         const response = await fetch(
             `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=${apiKey}&input=Down%20To%20The%20Details%20L.L.C.&inputtype=textquery`
-            //https://downtothedetailsllc-16467d68fd94.herokuapp.com/api/reviews?apiKey=AIzaSyBg8kkM413J-HbQpUUm0PM0FQ1hHk1jJNw&query=DownTo%20The%20Details
         );
 
         const data = await response.json();
@@ -28,21 +27,31 @@ app.get('/api/reviews', async (req, res) => {
 
         if (data.candidates && data.candidates.length > 0) {
             const placeId = data.candidates[0].place_id;
-            const detailsResponse = await fetch(
-                `https://maps.googleapis.com/maps/api/place/details/json?key=${apiKey}&place_id=ChIJN2hmQkVnbKYRs5gVMNjySYo&fields=reviews`
-            );
 
-            const detailsData = await detailsResponse.json();
+            // Initialize an empty array to collect reviews
+            let reviews = [];
+            let nextPageToken = null;
 
-            if (detailsData.result && detailsData.result.reviews) {
-                const reviews = detailsData.result.reviews;
+            // Use a loop to handle pagination and retrieve all reviews
+            do {
+                const detailsResponse = await fetch(
+                    `https://maps.googleapis.com/maps/api/place/details/json?key=${apiKey}&place_id=${placeId}&fields=reviews&page_token=${nextPageToken || ''}`
+                );
 
-                // Log the reviews data received on the server-side before sending to the client
-                console.log('Reviews data on the server-side:', reviews);
+                const detailsData = await detailsResponse.json();
 
-                res.json(reviews); // Send the reviews data to the client
-                return;
-            }
+                if (detailsData.result && detailsData.result.reviews) {
+                    reviews = [...reviews, ...detailsData.result.reviews];
+                }
+
+                nextPageToken = detailsData.next_page_token || null;
+            } while (nextPageToken);
+
+            // Log the reviews data received on the server-side before sending to the client
+            console.log('Reviews data on the server-side:', reviews);
+
+            res.json(reviews); // Send the reviews data to the client
+            return;
         }
 
         res.json([]); // Return an empty array if no reviews are found
