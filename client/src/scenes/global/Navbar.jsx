2552494ade7b5dc8f7855828461a80c229d92482
ton1savage <81397918+ton1savage@users.noreply.github.com>
2024-12-19 15:19:11 -0700

    fixed gallery done

--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -175,3 +175,5 @@ function Navbar() {
 }
 
 export default Navbar;
+
+
--- client/src/scenes/home/Gallery.jsx
+++ client/src/scenes/home/Gallery.jsx
@@ -1,173 +1,44 @@
-import React, { useState } from 'react';
-import { Box, Button, Typography } from "@mui/material";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import { Link as ScrollLink } from "react-scroll";
-import { useNavigate } from "react-router-dom";
-import { animateScroll as scroll } from "react-scroll";
-import { menu, close, navLinks } from "../../assets";
-import Gallery from '../home/Gallery';
+import React, { useRef, useEffect } from "react";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import { images } from "../../assets";
 
-const importAll = (r) =>
-    r.keys().reduce((acc, item) => {
-        acc[item.replace("./", "")] = r(item);
-        return acc;
-    }, {});
+const Gallery = ({ onClose }) => {
+    const galleryRef = useRef(null);
 
-export const heroTextureImports = importAll(
-    require.context("../../assets/logo", false, /\.(png|PNG|jpe?g|svg)$/)
-);
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (galleryRef.current && !galleryRef.current.contains(event.target)) {
+                onClose(); // Close the gallery when clicked outside
+            }
+        }
 
-
-function Navbar() {
-    const navigate = useNavigate();
-    const breakPoint = useMediaQuery("(min-width:560px)");
-    const breakPointTwo = useMediaQuery("(min-width:1090px)");
-    const showTypography = breakPointTwo; // Show Typography when breakPoint is false (screen width < 600px)
-    const [toggle, setToggle] = useState(false);
-    const [active, setActive] = useState('');
-    const logo = heroTextureImports["IMG_5789.PNG"];
-    const [showGallery, setShowGallery] = useState(false);
-
-    const toggleGallery = () => {
-        setShowGallery(!showGallery);
-    };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [onClose]);
 
     return (
-        <Box
-            display="flex"
-            alignItems="center"
-            width="100%"
-            backgroundColor="black"
-            color="black"
-            position="fixed"
-            top="0"
-            left="0"
-            zIndex="999"
-        >
-            <Box
-                width="90%"
-                margin="auto"
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-            >
-                <Box
-                    display='flex'
-                    alignItems='center'
-                    columnGap='20px'
-                    onClick={() => {
-                        navigate("/");
-                        scroll.scrollToTop(); // Scroll to top
-                    }}
-                    sx={{ "&:hover": { cursor: "pointer" } }}
+        <div className="gallery-overlay">
+            <div className="gallery-container" ref={galleryRef} style={{ padding: '10px' }}>
+                <ResponsiveMasonry
+                    columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
                 >
-                    <img src={logo} alt="Washing" style={{ width: '150px', height: '80px', objectFit: 'cover', borderRadius: '4px' }} />
-                </Box>
-                <Box display='flex' alignItems='center'>
-                    {!breakPoint && (<Box
-                        display="flex"
-                        zIndex="2"
-                    >
-                        (<img
-                            src={toggle ? close : menu}
-                            alt="menu"
-                            width='28px'
-                            height='28px'
-                            cursor="pointer"
-                            onClick={() => setToggle(!toggle)}
-                            zIndex='1000'
-                        />
-
-                        {toggle && (<Box
-                            display='flex'
-                            flexDirection='column'
-                            position='absolute'
-                            top='80px'
-                            right='0px'
-                            backgroundColor='black'
-                            alignItems='center'
-                            justifyContent='flex-start'
-                        >
-                            <ul
-                                style={{ listStyleType: 'none', textDecoration: 'none', margin: 0, padding: 0, marginBottom: -13 }}
-                            >
-                                {navLinks.map((link) => (
-                                    <li
-                                        key={link.id}
-                                        color={`${active === link.title ? '#ff9900' : 'white'}`}
-                                        onClick={() => {
-                                            setToggle(!toggle)
-                                            setActive(link.title)
-                                        }}
-
-                                        style={{ marginTop: '5px' }}
-                                    >
-                                        <ScrollLink to={link.id} smooth={true} duration={500} className="" offset={-100}>
-                                            <Typography style={{ color: active === link.title ? '#ff9900' : 'white', fontSize: '14px' }}>
-                                                {link.title}
-                                            </Typography>
-                                        </ScrollLink>
-                                    </li>
-                                ))};
-                            </ul>
-                            <Button style={{ borderRadius: '4px', border: '1px solid white', margin: '5px' }} onClick={toggleGallery} >
-                                <Typography variant='outlined' style={{ letterSpacing: '2px', color: 'white' }}>
-                                    VIEW GALLERY
-                                </Typography>
-                            </Button>
-                        </Box>
-                        )}
-                    </Box>
-                    )}
-                    {breakPoint && (
-                        <Box
-                            display='flex'
-                            flexDirection='row'
-                            zIndex='2'
-                        >
-                            <ul
-                                style={{ listStyleType: 'none', textDecoration: 'none', justifyContent: 'end', display: 'flex' }}
-                            >
-                                {navLinks.map((link) => (
-                                    <li
-                                        key={link.id}
-                                        color={`${active === link.title ? '#ff9900' : 'white'}`}
-                                        onClick={() => {
-                                            setToggle(!toggle)
-                                            setActive(link.title)
-                                        }}
-                                        style={{ marginRight: '15px', cursor: "pointer" }}
-                                    >
-                                        <ScrollLink to={link.id} smooth={true} duration={500} className="" offset={-100}>
-                                            <Typography style={{ color: active === link.title ? '#ff9900' : 'white', fontSize: '15px', fontWeight: '200', letterSpacing: '3px' }}>
-                                                {link.title}
-                                            </Typography>
-                                        </ScrollLink>
-                                    </li>
-                                ))};
-                            </ul>
-                        </Box>
-                    )}
-                    {breakPoint && (
-                        <Button
-                            style={{
-                                borderRadius: '4px',
-                                border: '1px solid white'
-                            }}
-                            onClick={toggleGallery}
-                        >
-                            <Typography variant='outlined' style={{ letterSpacing: '2px', color: 'white' }}>
-                                VIEW GALLERY
-                            </Typography>
-                        </Button>
-                    )}
-
-                    {/* Render Gallery component only when showGallery is true */}
-                    {showGallery && <Gallery onClose={toggleGallery} />}
-                </Box>
-            </Box>
-        </Box >
-    );
+                    <Masonry gutter="20px">
+                        {images.map((image, i) => (
+                            <div key={i} >
+                                <img
+                                    src={image}
+                                    style={{ width: '100%', maxHeight: '100%', display: 'block' }}
+                                    alt={`${i}`}
+                                />
+                            </div>
+                        ))}
+                    </Masonry>
+                </ResponsiveMasonry>
+            </div>
+        </div>
+    )
 }
 
-export default Navbar;
+export default Gallery;
